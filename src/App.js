e061728383b10195c51814c4d6cd307f602b5390
Kashish Saxena <kashish.saxena1306@gmail.com>
2023-07-07 02:40:09 +0530

    Static Files

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import HomeBanner from './components/HomeBanner';
 import Login from './components/Login';
 import Banner from './components/Banner';
 import List from './components/List';
+import { BrowserRouter as Router,  Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <React.Fragment>
-      <Header/>
-      <Banner/>
-      <List/>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <React.Fragment>
+              <Header/>
+              <HomeBanner/>
+            </React.Fragment>
+          }/>
+          <Route path="/login" element={
+              <Login/>
+          }/>
+          <Route path="/dashboard" element={
+              <React.Fragment>
+                <Header/>
+                <Banner/>
+                <List/>
+              </React.Fragment>
+          }/>
+        </Routes>
+      </Router>
     </React.Fragment>
   );
 }
