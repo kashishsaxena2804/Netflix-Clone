fedaf5cba167350f92df845194467fcd07f8e910
Kashish Saxena <kashish.saxena1306@gmail.com>
2023-07-10 21:56:04 +0530

    Static Files

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,65 +1,125 @@
-import { useNavigate, Link } from "react-router-dom";
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { firebaseConfig } from "./firebaseConfig" ; 
-import { useState } from "react";
-import { error } from "jquery";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import { initializeApp } from 'firebase/app';
+import { getAuth, 
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from "firebase/auth";
+import { firebaseConfig } from './firebaseConfig.js';
+import { useEffect, useState } from "react";
 
-const Login = ( {page} ) => {
-  const app = initializeApp(firebaseConfig);
+const Login = () => {
+  initializeApp(firebaseConfig);
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isUserExist, setUserExist ] = useState(false);
+  const location = useLocation();
+  const page = location.pathname === '/login' ? true:false;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [ isUserExist, setUserExist ] = useState(false);
+  const [ isEmailUsed, setIsEmailUsed] = useState(false);
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
 
   const auth = getAuth();
 
-  const onSignInClickHandler = (e) =>{
+  const validation = (fieldName, value) => {
+    switch(fieldName) {
+      case 'email':
+        return value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+      case 'password':
+        return value.length >= 6;
+      default:
+        break;
+    }
+  };
+
+  const ctaClickHandler = (e) => {
     e.preventDefault();
-    signInWithEmailAndPassword(auth,email, password)
-    .then(auth => {
-      if(auth){
-        navigate('/dashboard');
-      }
-    })
-    .catch (error => setUserExist(true));
-    //user-not-found
-  }
 
+    if(!validation('email', email) || !validation('password', password)){
+      setEmailValid(validation('email', email));
+      setPasswordValid(validation('password', password));
+      return;
+    }
+
+    if(page){
+      signInWithEmailAndPassword(auth, email, password)
+      .then( auth => {
+        if(auth){
+          navigate('/dashboard');
+        }
+      })
+      .catch( error => setUserExist(true));
+      // user-not-found
+    }else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then(
+        auth => {
+          if(auth){
+            navigate('/dashboard');
+          }
+        })
+        .catch( error => setIsEmailUsed(true));
+    }
+  };
+
+  useEffect(()=>{
+    setUserExist(false);
+    setIsEmailUsed(false);
+  },[location]);
   const emailOnChangeHandler = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
-    return (
-      <div className="login">
-        <div className="holder">
-          <h1 className="text-white">{ page ? 'Sign In' : 'Register' }</h1>
+  return(
+    <div className="login">
+      <div className="holder">
+        <h1 className="text-white">{ page ? 'Sign In' : 'Register'}</h1>
+        <br/>
+        <form>
+          <input 
+            className="form-control" 
+            value={email} 
+            onChange={emailOnChangeHandler} 
+            type="email" 
+            placeholder="Email"/>
+          { !emailValid && <p className="text-danger">Email is invalid/blank</p> }
+          <input 
+            className="form-control"
+            value={password} 
+            onChange={(e)=>setPassword(e.target.value)} 
+            type="password" 
+            placeholder="Password"/>
+          { !passwordValid && <p className="text-danger">Password is invalid/blank</p>}
+          <button className="btn btn-danger btn-block" onClick={ctaClickHandler}>
+            { page ? 'Sign In' : 'Register'}
+          </button>
           <br/>
-          <form>
-            <input className="form-control " value={email} onChange={emailOnChangeHandler} type="email" placeholder="Email"/>
-            <input className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password"/>
-            <button className="btn btn-danger btn-block" onClick={onSignInClickHandler}>{ page ? 'Sign In' : 'Register' }</button>
-            <br/>
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-              <label className="form-check-label text-white" for="flexCheckDefault">
-                Remember Me
-              </label>
-            </div>
-          </form>
-          <br/>
-          <br/>
-          { isUserExist && <p className="text-danger">Sorry, we can't find an account with this email address. Please try again or create a new account.</p>}
-          <div className="login-form-other">
-            <div className="login-signup-now">{ page ? 'New to Netflix?' : 'Existing User' } &nbsp;
-              <Link className=" "  to="/">{ page ? 'Sign up now' : 'Sign In' }</Link>
-            </div>
+          {
+            page && <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+            <label className="form-check-label text-white" htmlFor="flexCheckDefault">
+              Remember Me
+            </label>
+          </div>
+          }
+        </form>
+        <br/>
+        <br/>
+        { isUserExist && <p className="text-danger">User does not exist | Go for Signup</p> }
+        { isEmailUsed && <p className="text-danger">Email already in use | Go for Sign In</p> }
+        <div className="login-form-other">
+          <div className="login-signup-now">
+          { page ? 'New to Netflix?' : 'Existing User'} &nbsp;
+            <Link className=" " to={page ? '/register' : '/login'}>
+              { page ? 'Sign up now' : 'Sign In'}
+            </Link>.
           </div>
         </div>
-        <div className="shadow"></div>
-        <img className="concord-img vlv-creative" src="https://assets.nflxext.com/ffe/siteui/vlv3/ce221d7a-8901-41d9-b726-3ea2efe3a650/2f69dcef-463f-44da-b3b9-662b65f33f82/IN-en-20230703-popsignuptwoweeks-perspective_alpha_website_large.jpg" srcset="" alt=""></img>
       </div>
-    )
-  }
-  
-  export default Login;
+      <div className="shadow"></div>
+      <img className="concord-img vlv-creative" src="https://assets.nflxext.com/ffe/siteui/vlv3/6e32b96a-d4be-4e44-a19b-1bd2d2279b51/ee068656-14b9-4821-89b4-53b4937d9f1c/IN-en-20220516-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Login;
